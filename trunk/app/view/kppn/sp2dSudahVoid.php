b570ff633a8b6f2f23621e1a24893a4b17111978
commandcenter.span@gmail.com <commandcenter.span@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-17 04:36:56 +0000

    view update v2

--- trunk/app/view/kppn/sp2dSudahVoid.php
+++ trunk/app/view/kppn/sp2dSudahVoid.php
@@ -49,14 +49,14 @@
 
 
 <div id="fitur">
-		<table class="table-bordered zebra scroll" width="100%">
+		<table width="100%" class="table table-bordered zebra scroll">
             <!--baris pertama-->
 			<thead>
 					<th>No.</th>
-					<th>Tanggal SP2D</th>
+					<th>Tgl SP2D</th>
 					<th>No. SP2D</th>
-					<th>Status</th>
-					<th>Tanggal Create</th>
+					<!--th>Status</th-->
+					<th>TglSelesai SP2D</th>
 					<th>No. Transaksi</th>
 					<th>No. Invoice</th>
 					<th>Jumlah Rp</th>
@@ -80,7 +80,7 @@
 							echo "<td>" . $no++ . "</td>";
 							echo "<td>" . $value->get_payment_date() . "</td>";
 							echo "<td>" . $value->get_check_number() . "</td>";
-							echo "<td>" . $value->get_return_code() . "</td>";
+							//echo "<td>" . $value->get_return_code() . "</td>";
 							echo "<td>" . $value->get_creation_date() . "</td>";
 							echo "<td>" . $value->get_check_number_line_num() . "</td>";
 							echo "<td>" . $value->get_invoice_num() . "</td>";
