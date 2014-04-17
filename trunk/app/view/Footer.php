d8639ed722b46642e22b3330c5af5b62d2f3f8ac
commandcenter.span@gmail.com <commandcenter.span@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-17 08:09:26 +0000

    view footer v1

--- trunk/app/view/Footer.php
+++ trunk/app/view/Footer.php
@@ -5,9 +5,15 @@
 		<div><h2 style="border-bottom: 1px solid #eee; padding-bottom: 10px">Developer :</h2>
 		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 4px; margin-top: 0px"></i></a>
 			<div id="top">
-				<h3><a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">1. Andi Saputra</a> - 198509112007101001 - Kanwil Kepulauan Riau </br> 
-                <a href="mailto:mashtree@gmail.com" target="_top">2. Triyono</a> -  198205152001121001 - AKKP Setditjen</br>    
-                <a href="mailto:nez817@gmail.com" target="_top">3. Ernestina R</a> - 198401282003122001 - Kanwil Jawa Tengah</h3>
+				<h3>
+				<a href="mailto:doniebelva@gmail.com" target="_top">Donie Mahaputra</a> - Project Manager</br>
+				<a href="mailto:turbocharge90@gmail.com" target="_top">Rayman Hidayat</a> - Project Manager</br>
+				<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">Andi Saputra</a> - Application Engineer</br>
+				<a href="mailto:hkm.lutfi@gmail.com" target="_top">Luthfi Hakim</a> - Application Modeller</br>
+                <a href="mailto:nez817@gmail.com" target="_top">Ernestina R</a> - User Interface Designer</br>
+				<a href="mailto:iq84l.dean@gmail.com" target="_top">M. Iqbal Anshori</a> - Database Designer</br>
+				<a href="mailto:boys2641@gmail.com" target="_top">Agus Priyono</a> - Server Administrator</br>
+				</h3>
             </div>
         </div>
     </div>
