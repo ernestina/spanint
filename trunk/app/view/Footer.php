da205e3102f9dd4e57facf43c3742da76d0a5a09
commandcenter.span@gmail.com <commandcenter.span@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-21 09:42:53 +0000

    paling penting!

--- trunk/app/view/Footer.php
+++ trunk/app/view/Footer.php
@@ -5,8 +5,8 @@
 		<div><h2 style="border-bottom: 1px solid #eee; padding-bottom: 10px">Developer :</h2>
 		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 5px; margin-top: 2px"></i></a>
 			<div id="top">
-				<h3>
-				<a href="mailto:turbocharge90@gmail.com" target="_top">Rayman Hidayat</a> - Project Coordinator</br>
+				<h3 style="text-align: center">
+				<a href="mailto:turbocharge90@gmail.com" target="_top">Rinaldi Hidayat</a> - Project Coordinator</br>
 				<a href="mailto:doniebelva@gmail.com" target="_top">Donie Mahaputra</a> - Project Manager and System Analyst</br>
 				<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">Andi Saputra</a> - Application Engineer</br>
 				<a href="mailto:nez817@gmail.com" target="_top">Ernestina R</a> - User Interface Designer</br>
@@ -14,9 +14,9 @@
 				<a href="mailto:baygiv@gmail.com" target="_top">Bayu Yudhistira</a> - App. Designer Module UM</br>
 				<a href="mailto:achmadford@gmail.com" target="_top">Achmad Ford</a> - App. Designer Module GR</br>
 				<a href="mailto:hasan.arie@gmail.com" target="_top">Arie Hasan</a> - App. Designer Module XICO</br>
-				<a href="mailto:iq84l.dean@gmail.com" target="_top">M. Iqbal Anshori</a> - Database Designer</br>
-				<a href="mailto:boys2641@gmail.com" target="_top">Agus Priyono</a> - Server Developer and Operator</br>
-				<a href="mailto:cakyus@gmail.com" target="_top">Yusuf AB</a> - Server  Developer and Operator</br>
+				<a href="mailto:iq84l.dean@gmail.com" target="_top">M. Iqbal Anshori</a> - Database Administrator</br>
+				<a href="mailto:boys2641@gmail.com" target="_top">Agus Priyono</a> - System Administrator</br>
+				<a href="mailto:cakyus@gmail.com" target="_top">Yusuf AB</a> - System Administrator</br>
 				</h3>
             </div>
         </div>
