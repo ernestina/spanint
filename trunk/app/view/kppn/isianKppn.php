422f5e5fed35af2f23179ee25ed89ac57de1fa4a
commandcenter.span@gmail.com <commandcenter.span@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-21 01:56:30 +0000

    view v3

--- trunk/app/view/kppn/isianKppn.php
+++ trunk/app/view/kppn/isianKppn.php
@@ -92,7 +92,7 @@
 			$no=1;
 			if (isset($this->data)){
 				if (empty($this->data)){
-					echo "Tidak ada data";
+					echo "<div class='alert alert-danger'><strong>Info! </strong>Tidak ada data.</div>";
 				} else {
 					foreach ($this->data as $value){ 
 						echo "<tr>	";
@@ -115,7 +115,7 @@
 					}
 				} 
 			} else {
-				echo "Silakan masukan filter";
+				echo "<div class='alert alert-info'><strong>Info! </strong>Silakan masukan filter.</div>";
 			}
 			?>
 			</tbody>
@@ -252,7 +252,7 @@
             jml++;
         }
 		
-		if(v_kdsatker !='' && !v_kdsatker.match(pattern)){
+		if(v_kdsatker !=''  && !v_kdsatker.match(pattern)){
             var wsatker = 'No Transaksi harus dalam bentuk angka!';
             $('#wsatker').html(wbarsp2d);
             $('#wsatker').fadeIn(200);
