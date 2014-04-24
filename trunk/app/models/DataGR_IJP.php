82dc729605359ca5e279f5a0e6006f4669184323
commandcenter.span@gmail.com <commandcenter.span@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-24 11:01:28 +0000

    view v4.2 + modul GR-IJP

--- trunk/app/models/DataGR_IJP.php
+++ trunk/app/models/DataGR_IJP.php
@@ -9,13 +9,22 @@ class DataGR_IJP{
 
     private $db;
 	private $_kppn;
-    private $_gl_date_char;
+    private $_bulan;
+	//private $_gl_date_char;
     private $_bank_code;
-    private $_bank_branch_code;
+    //private $_bank_branch_code;
 	private $_bank_account_num;
-	private $_transaksi;
-	private $_baris;
-    private $_table1 = 'spgr_mpn_receipts_all_v';
+	//private $_transaksi;
+	//private $_baris;
+	private $_n01; private $_n02; private $_n03; private $_n04; private $_n05;
+	private $_n06; private $_n07; private $_n08; private $_n09; private $_n10; 
+	private $_n11; private $_n12; private $_n13; private $_n14; private $_n15;
+	private $_n16; private $_n17; private $_n18; private $_n19; private $_n20;
+	private $_n21; private $_n22; private $_n23; private $_n24; private $_n25;
+	private $_n26; private $_n27; private $_n28; private $_n29; private $_n30;
+	private $_n31;
+	private $_jumlah;
+    private $_table1 = 'SPGR_MPN_RECEIPTS_ALL_V_RIJP';
     public $registry;
 
     /*
@@ -38,7 +47,7 @@ class DataGR_IJP{
 				FROM " 
 				. $this->_table1. " 
 				 WHERE 
-				KPPN = ".Session::get('id_user')
+				KPPN = '".Session::get('id_user')."'"
 				
 				;
 		$no=0;
@@ -51,12 +60,45 @@ class DataGR_IJP{
         foreach ($result as $val) {
             $d_data = new $this($this->registry);
             $d_data->set_kppn($val['KPPN']);
-            $d_data->set_gl_date_char($val['GL_DATE_CHAR']);
+			$d_data->set_bulan($val['BULAN']);
+            //$d_data->set_gl_date_char($val['GL_DATE_CHAR']);
 			$d_data->set_bank_code($val['BANK_CODE']);
-            $d_data->set_bank_branch_code($val['BANK_BRANCH_CODE']);
+            //$d_data->set_bank_branch_code($val['BANK_BRANCH_CODE']);
             $d_data->set_bank_account_num($val['BANK_ACCOUNT_NUM']);
-			$d_data->set_transaksi($val['TRANSAKSI']);
-			$d_data->set_baris($val['BARIS']);
+			//$d_data->set_transaksi($val['TRANSAKSI']);
+			//$d_data->set_baris($val['BARIS']);
+			$d_data->set_n01(number_format($val['01']));
+			$d_data->set_n02(number_format($val['02']));
+			$d_data->set_n03(number_format($val['03']));
+			$d_data->set_n04(number_format($val['04']));
+			$d_data->set_n05(number_format($val['05']));
+			$d_data->set_n06(number_format($val['06']));
+			$d_data->set_n07(number_format($val['07']));
+			$d_data->set_n08(number_format($val['08']));
+			$d_data->set_n09(number_format($val['09']));
+			$d_data->set_n10(number_format($val['10']));
+			$d_data->set_n11(number_format($val['11']));
+			$d_data->set_n12(number_format($val['12']));
+			$d_data->set_n13(number_format($val['13']));
+			$d_data->set_n14(number_format($val['14']));
+			$d_data->set_n15(number_format($val['15']));
+			$d_data->set_n16(number_format($val['16']));
+			$d_data->set_n17(number_format($val['17']));
+			$d_data->set_n18(number_format($val['18']));
+			$d_data->set_n19(number_format($val['19']));
+			$d_data->set_n20(number_format($val['20']));
+			$d_data->set_n21(number_format($val['21']));
+			$d_data->set_n22(number_format($val['22']));
+			$d_data->set_n23(number_format($val['23']));
+			$d_data->set_n24(number_format($val['24']));
+			$d_data->set_n25(number_format($val['25']));
+			$d_data->set_n26(number_format($val['26']));
+			$d_data->set_n27(number_format($val['27']));
+			$d_data->set_n28(number_format($val['28']));
+			$d_data->set_n29(number_format($val['29']));
+			$d_data->set_n30(number_format($val['30']));
+			$d_data->set_n31(number_format($val['31']));
+			$d_data->set_jumlah(number_format($val['JUMLAH']));
 			$data[] = $d_data;
         }
         return $data;
@@ -69,28 +111,111 @@ class DataGR_IJP{
     public function set_kppn($kppn) {
         $this->_kppn = $kppn;
     }
-	
-    public function set_gl_date_char($gl_date_char) {
-        $this->_gl_date_char = $gl_date_char;
+	public function set_bulan($bulan) {
+        $this->_bulan = $bulan;
     }
-	
-	
     public function set_bank_code($bank_code) {
         $this->_bank_code = $bank_code;
     }
-	
     public function set_bank_account_num($bank_account_num) {
         $this->_bank_account_num = $bank_account_num;
     }
-	public function set_bank_branch_code($bank_branch_code) {
-        $this->_bank_branch_code = $bank_branch_code;
+	public function set_n01($n01) {
+        $this->_n01 = $n01;
     }
-	public function set_transaksi($transaksi) {
-        $this->_transaksi = $transaksi;
+	public function set_n02($n02) {
+        $this->_n02 = $n02;
     }
-	public function set_baris($baris) {
-        $this->_baris = $baris;
-    }	
+	public function set_n03($n03) {
+        $this->_n03 = $n03;
+    }
+	public function set_n04($n04) {
+        $this->_n04 = $n04;
+    }
+	public function set_n05($n05) {
+        $this->_n05 = $n05;
+    }
+	public function set_n06($n06) {
+        $this->_n06 = $n06;
+    }
+	public function set_n07($n07) {
+        $this->_n07 = $n07;
+    }
+	public function set_n08($n08) {
+        $this->_n08 = $n08;
+    }
+	public function set_n09($n09) {
+        $this->_n09 = $n09;
+    }
+	public function set_n10($n10) {
+        $this->_n10 = $n10;
+    }
+	public function set_n11($n11) {
+        $this->_n11 = $n11;
+    }
+	public function set_n12($n12) {
+        $this->_n12 = $n12;
+    }
+	public function set_n13($n13) {
+        $this->_n13 = $n13;
+    }
+	public function set_n14($n14) {
+        $this->_n14 = $n14;
+    }
+	public function set_n15($n15) {
+        $this->_n15 = $n15;
+    }
+	public function set_n16($n16) {
+        $this->_n16 = $n16;
+    }
+	public function set_n17($n17) {
+        $this->_n17 = $n17;
+    }
+	public function set_n18($n18) {
+        $this->_n18 = $n18;
+    }
+	public function set_n19($n19) {
+        $this->_n19 = $n19;
+    }
+	public function set_n20($n20) {
+        $this->_n20 = $n20;
+    }
+	public function set_n21($n21) {
+        $this->_n21 = $n21;
+    }
+	public function set_n22($n22) {
+        $this->_n22 = $n22;
+    }
+	public function set_n23($n23) {
+        $this->_n23 = $n23;
+    }
+	public function set_n24($n24) {
+        $this->_n24 = $n24;
+    }
+	public function set_n25($n25) {
+        $this->_n25 = $n25;
+    }
+	public function set_n26($n26) {
+        $this->_n26 = $n26;
+    }
+	public function set_n27($n27) {
+        $this->_n27 = $n27;
+    }
+	public function set_n28($n28) {
+        $this->_n28 = $n28;
+    }
+	public function set_n29($n29) {
+        $this->_n29 = $n29;
+    }
+	public function set_n30($n30) {
+        $this->_n30 = $n30;
+    }
+	public function set_n31($n31) {
+        $this->_n31 = $n31;
+    }
+	public function set_jumlah($jumlah) {
+        $this->_jumlah = $jumlah;
+    }
 	/*
      * getter
      */
@@ -98,27 +223,110 @@ class DataGR_IJP{
 	public function get_kppn() {
         return $this->_kppn;
     }
-	
-	public function get_gl_date_char() {
-        return $this->_gl_date_char;
+	public function get_bulan() {
+        return $this->_bulan;
     }
-	
 	public function get_bank_code() {
         return $this->_bank_code;
-    }
-	
-	
-	public function get_bank_branch_code() {
-        return $this->_bank_branch_code;
     }
 	public function get_bank_account_num() {
         return $this->_bank_account_num;
     }
-	public function get_transaksi() {
-        return $this->_transaksi;
+	public function get_n01() {
+        return $this->_n01;
     }
-	public function get_baris() {
-        return $this->_baris;
+	public function get_n02() {
+        return $this->_n02;
+    }
+	public function get_n03() {
+        return $this->_n03;
+    }
+	public function get_n04() {
+        return $this->_n04;
+    }
+	public function get_n05() {
+        return $this->_n05;
+    }
+	public function get_n06() {
+        return $this->_n06;
+    }
+	public function get_n07() {
+        return $this->_n07;
+    }
+	public function get_n08() {
+        return $this->_n08;
+    }
+	public function get_n09() {
+        return $this->_n09;
+    }
+	public function get_n10() {
+        return $this->_n10;
+    }
+	public function get_n11() {
+        return $this->_n11;
+    }
+	public function get_n12() {
+        return $this->_n12;
+    }
+	public function get_n13() {
+        return $this->_n13;
+    }
+	public function get_n14() {
+        return $this->_n14;
+    }
+	public function get_n15() {
+        return $this->_n15;
+    }
+	public function get_n16() {
+        return $this->_n16;
+    }
+	public function get_n17() {
+        return $this->_n17;
+    }
+	public function get_n18() {
+        return $this->_n18;
+    }
+	public function get_n19() {
+        return $this->_n19;
+    }
+	public function get_n20() {
+        return $this->_n20;
+    }
+	public function get_n21() {
+        return $this->_n21;
+    }
+	public function get_n22() {
+        return $this->_n22;
+    }
+	public function get_n23() {
+        return $this->_n23;
+    }
+	public function get_n24() {
+        return $this->_n24;
+    }
+	public function get_n25() {
+        return $this->_n25;
+    }
+	public function get_n26() {
+        return $this->_n26;
+    }
+	public function get_n27() {
+        return $this->_n27;
+    }
+	public function get_n28() {
+        return $this->_n28;
+    }
+	public function get_n29() {
+        return $this->_n29;
+    }
+	public function get_n30() {
+        return $this->_n30;
+    }
+	public function get_n31() {
+        return $this->_n31;
+    }
+	public function get_jumlah() {
+        return $this->_jumlah;
     }
 
     /*
