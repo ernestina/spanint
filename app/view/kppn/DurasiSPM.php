f7ec1f39f8f8c8cc37766cd593bca33bdb4a0d32
hkm.lutfi@gmail.com <hkm.lutfi@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-28 10:13:48 +0000

    DURASI SPM

---
+++ app/view/kppn/DurasiSPM.php
@@ -0,0 +1,178 @@
+<div id="top">
+	<div id="header">
+        <h2>DURASI PEMROSESAN SPM - SP2D <?php //echo $nama_satker; ?> <?php //echo $kode_satker; ?><br>
+			<?php echo Session::get('user'); ?>
+		</h2>
+    </div>
+
+<a href="#zModal" class="modal">FILTER DATA</a><br><br>
+        <div id="zModal" class="modalDialog" >
+            <div>
+                <h2 style="border-bottom: 1px solid #eee; padding-bottom: 10px">FILTER</h2>
+				<a href="<?php
+                    $_SERVER['PHP_SELF'];
+                ?>" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 5px; margin-top: 2px"></i>
+</a>
+	
+	<div id="top">
+	<form method="POST" action="DurasiSpm" enctype="multipart/form-data">
+		
+		<div id="winvoice" class="error"></div>
+		<label class="isian">No Invoice: </label>
+		<input type="text" name="invoice" id="invoice" value="<?php if (isset($this->invoice)){echo $this->invoice;}?>">
+		
+		<div id="winvoice" class="error"></div>
+		<label class="isian">Kode Satker: </label>
+		<input type="text" name="kdsatker" id="kdsatker" value="<?php if (isset($this->kdsatker)){echo $this->kdsatker;}?>">
+		
+		<label class="isian">Jenis SPM: </label>
+		<select type="text" name="JenisSPM" id="JenisSPM">
+			<option value=''>- pilih -</option>
+			<option value='GAJI INDUK' <?php if ($this->status=="GAJI INDUK"){echo "GAJI INDUK";}?>>GAJI INDUK</option>
+			<option value='GAJI SUSULAN' <?php if ($this->status=="GAJI SUSULAN"){echo "GAJI SUSULAN";}?>>GAJI SUSULAN</option>
+			<option value='GAJI LAINNYA' <?php if ($this->status=="GAJI LAINNYA"){echo "GAJI LAINNYA";}?>>GAJI LAINNYA</option>	
+			<option value='LANGSUNG' <?php if ($this->status=="GAJI LAINNYA"){echo "GAJI LAINNYA";}?>>LANGSUNG</option>	
+			<option value='LS-KONTRAKTUAL' <?php if ($this->status=="LS-KONTRAKTUAL"){echo "LS-KONTRAKTUAL";}?>>LS-KONTRAKTUAL</option>
+			<option value='DANA UP (UYHD)' <?php if ($this->status=="DANA UP (UYHD)"){echo "DANA UP (UYHD)";}?>>DANA UP (UYHD)</option>
+			<option value='GANTI UP' <?php if ($this->status=="GANTI UP"){echo "GANTI UP";}?>>GANTI UP</option>
+		</select>
+		
+		<label class="isian">Durasi: </label>	
+		<select type="text" name="durasi" id="durasi">
+			<option value=''>- pilih -</option>
+			<option value="< '1" <?php if ($this->durasi=="< 1"){echo "Kurang dari 1 jam";}?>>Kurang dari satu jam</option>
+			<option value="> '1" <?php if ($this->status=="> 1"){echo "Lebih dari 1 jam";}?>>Lebih dari satu jam</option>
+			<option value="> '24" <?php if ($this->durasi=="> 24"){echo "Lebih dari 1 hari";}?>>Lebih dari satu hari</option>
+			
+		</select>
+
+		<div id="wtgl" class="error"></div>
+		<label class="isian">Tanggal: </label>
+		<ul class="inline">
+		<li><input type="text" class="tanggal" name="tgl_awal" id="tgl_awal" value="<?php if (isset($this->d_tgl_awal)){echo $this->d_tgl_awal;}?>" /> </li> <li>s/d</li>
+		<li><input type="text" class="tanggal" name="tgl_akhir" id="tgl_akhir" value="<?php if (isset($this->d_tgl_akhir)){echo $this->d_tgl_akhir;}?>"></li>
+		</ul>
+
+		<input type="hidden" name="kd_satker" id="kd_satker" value="<?php echo $kode_satker; ?>">
+		<input type="hidden" name="kd_kppn" id="kd_kppn" value="<?php echo $kode_kppn; ?>">
+		<input type="hidden" name="kd_adk_name" id="kd_adk_name" value="<?php echo $_FILES['fupload']['name']; ?>">
+		<input type="hidden" name="kd_jml_pdf" id="kd_jml_pdf" value="<?php echo '10'; ?>">
+		<input type="hidden" name="kd_file_name" id="kd_file_name" value="<?php echo $kode_satker."_".$kode_kppn."_".date("d-m-y")."_"; ?>">
+		<!--input id="submit" class="sukses" type="submit" name="submit_file" value="SIMPAN" onClick=""-->
+
+		<ul class="inline" style="margin-left: 130px">
+		<li><input id="reset" class="normal" type="reset" name="reset_file" value="RESET" onClick=""></li>
+		<li><input id="submit" class="sukses" type="submit" name="submit_file" value="CARI" onClick=""></li>
+		<!--onClick="konfirm(); return false;"-->
+		</ul>
+	</form>
+</div>
+</div>
+</div>
+
+
+
+<div id="fitur">
+		<table width="100%" class="table table-bordered zebra scroll">
+            <!--baris pertama-->
+			<thead>
+					<th>No.</th>
+					<th>Nomor Invoice</th>
+					<!--th>KPPN</th-->
+					<th>Jenis SPM</th>
+					<th>Tanggal Upload</th>
+					<th>Jam Upload</th>
+					<th>Nomor SP2D</th>
+					<th>Tanggal Selesai SP2D</th>
+					<th>Jam Selesai SP2D</th>
+					<th>Durasi</th>
+					
+			</thead>
+			<tbody>
+			<?php 
+			$no=1;
+			//var_dump ($this->data);
+			if (isset($this->data)){
+				if (empty($this->data)){
+					echo "Tidak ada data";
+				} else {
+					foreach ($this->data as $value){ 
+					echo "<tr>	";
+						echo "<td>" . $no++ . "</td>";
+						echo "<td>" . $value->get_invoice_num() . "</td>";
+						echo "<td>" . $value->get_attribute1() . "</td>";
+						echo "<td>" . $value->get_aia_creation_date() . "</td>";
+						echo "<td>" . $value->get_jam_upload() . "</td>";
+						echo "<td>" . $value->get_check_number() . "</td>";
+						echo "<td>" . $value->get_aca_creation_date() . "</td>";
+						echo "<td>" . $value->get_jam_selesai_sp2d() . "</td>";
+						echo "<td>" . $value->get_durasi() . "</td>";
+						
+					echo "</tr>	";
+					}
+				}
+			} else {
+				echo "Silakan masukan filter";
+			}
+			?>
+			</tbody>
+        </table>
+		</div>
+</div>
+
+<script type="text/javascript">
+	$(function(){
+        hideErrorId();
+        hideWarning();
+		
+		$("#tgl_awal").datepicker({dateFormat: "dd-mm-yy"
+		});
+		
+		$("#tgl_akhir").datepicker({dateFormat: "dd-mm-yy"
+		});
+    });
+
+    
+    function hideErrorId(){
+        $('.error').fadeOut(0);
+    }
+
+    function hideWarning(){
+		$('#invoice').keyup(function(){
+            if(document.getElementById('invoice').value !=''){
+                $('#winvoice').fadeOut(200);
+            }
+        });
+		$('#tgl_awal').change(function(){
+            if(document.getElementById('tgl_awal').value !='' && document.getElementById('tgl_akhir').value !=''){
+                $('#wtgl').fadeOut(200);
+            } 
+        });
+		
+		$('#tgl_akhir').change(function(){
+            if(document.getElementById('tgl_awal').value !='' && document.getElementById('tgl_akhir').value !=''){
+                $('#wtgl').fadeOut(200);
+            } 
+        });
+
+    }
+    
+    function cek_upload(){
+		var v_invoice = document.getElementById('invoice').value;
+		var v_tglawal = document.getElementById('tgl_awal').value;
+		var v_tglakhir = document.getElementById('tgl_akhir').value;
+		
+        var jml = 0;
+        if(v_invoice=='' && v_tglawal=='' && v_tglakhir==''){
+            $('#winvoice').html('Harap isi no invoice');
+            $('#winvoice').fadeIn();
+			$('#wtgl').html('Harap isi tanggal');
+            $('#wtgl').fadeIn();
+            jml++;
+        }
+		
+        if(jml>0){
+            return false;
+        } 
+    }
+</script>
