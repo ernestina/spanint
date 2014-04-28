972bf86c90f36a344ab0f7634e8d7caf57dd0270
hkm.lutfi@gmail.com <hkm.lutfi@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-04-28 10:15:11 +0000

    DATA DURASI

--- trunk/app/controllers/DataSPMController.php
+++ trunk/app/controllers/DataSPMController.php
@@ -43,9 +43,13 @@ class DataSPMController extends BaseController {
 					$filter[$no++]=$_POST['tgl_awal'];
 					$filter[$no++]=$_POST['tgl_akhir'];
 				}
-				
-			}	
-		$this->view->data = $d_spm1->get_hist_spm_filter ($filter);
+				$this->view->data = $d_spm1->get_hist_spm_filter ($filter);
+			}
+			else{	
+				$filter[$no++]=" STATUS = 'OPEN'";
+		
+				$this->view->data = $d_spm1->get_hist_spm_filter ($filter);
+			}
 		//var_dump($d_spm->get_hist_spm_filter());
 		$this->view->render('kppn/posisiSPM');
 	}
@@ -103,13 +107,48 @@ class DataSPMController extends BaseController {
 				}
 			$this->view->data = $d_spm1->get_history_spm_filter ($filter);
 			}	
-		
+			
+			
 		//var_dump($d_spm->get_hist_spm_filter());
 		$this->view->render('kppn/historySPM');
 	}
 	
 	
+	public function DurasiSpm() {
+		$d_spm1 = new DataDurasiSPM($this->registry);
+		$filter = array ();
+		$no=0;
+			if (isset($_POST['submit_file'])) {
+				
+				if ($_POST['invoice']!=''){
+					$filter[$no++]="invoice_num = '".$_POST['invoice'] . "'";
+				}
+				if ($_POST['kdsatker']!=''){
+					$filter[$no++]="substr(invoice_num,8,6) = '".$_POST['kdsatker'] . "'";
+				}
+				if ($_POST['JenisSPM']!=''){
+					$filter[$no++]="jendok = '".$_POST['JenisSPM'] . "'";
+				}
+				if ($_POST['durasi']!=''){
+					$filter[$no++]="durasi2 ".$_POST['durasi'] . "'";
+				}
+				
+				if ($_POST['tgl_awal']!='' AND $_POST['tgl_akhir']!=''){
+					$filter[$no++] = "TANGGAL_UPLOAD BETWEEN '".$_POST['tgl_awal']."' AND '".$_POST['tgl_akhir']."'";
+					$this->view->d_tgl_awal = $_POST['tgl_awal'];
+					$this->view->d_tgl_akhir = $_POST['tgl_akhir'];
+				}
+			$this->view->data = $d_spm1->get_durasi_spm_filter ($filter);	
+			}
+		ELSE {	
+		$filter[$no++] = " ROWNUM <= '100' ";
+		$this->view->data = $d_spm1->get_durasi_spm_filter ($filter);
+		//var_dump($d_spm1->get_error_spm_filter ($filter));
+		}
+		$this->view->render('kppn/DurasiSPM');
+	}
 	
+			
 	
     public function __destruct() {
         
