92609cfff85a6e7377b72eac9f25e824d2e2f70b
hkm.lutfi@gmail.com <hkm.lutfi@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-05-12 07:55:10 +0000

    ini buat yang PFK

---
+++ app/models/DataPFK.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class DataPFK{
+
+    private $db;
+	private $_akun;
+	private $_potongan_spm;
+	private $_setoran_mpn;
+	private $_total;
+	private $_table1 = 'PFK_SPAN_PVT';
+    public $registry;
+
+    /*
+     * konstruktor
+     */
+
+    public function __construct($registry = Registry) {
+        $this->db = $registry->db;
+        $this->registry = $registry;
+    }
+
+    /*
+     * mendapatkan data dari tabel Data Tetap
+     * @param limit batas default null
+     * return array objek Data Tetap*/
+    
+    public function get_gr_pfk_filter($filter) {
+		Session::get('id_user');
+		$no=0;
+		foreach ($filter as $filter);
+		$sql = "select 
+				akun,
+				case when trx = 1 then ".$filter." * (-1) end potongan_spm,
+				case when trx = 2 then ".$filter." * (-1) end setoran_mpn
+				from " 
+				. $this->_table1. "
+				WHERE kppn = ".Session::get('id_user')
+				
+				;
+				
+		/*$no=0;
+		foreach ($filter as $filter) {
+			$sql .= " AND ".$filter;
+		}*/
+		//var_dump ($sql);
+		$sql .= " ORDER BY akun";
+		
+		
+		
+        $result = $this->db->select($sql);
+        $data = array();   
+        foreach ($result as $val) {
+            $d_data = new $this($this->registry);
+            $d_data->set_akun($val['AKUN']);
+			$d_data->set_potongan_spm($val['POTONGAN_SPM']);
+            $d_data->set_setoran_mpn($val['SETORAN_MPN']);
+			$d_data->set_total($val['SETORAN_MPN']+$val['POTONGAN_SPM']);
+            $data[] = $d_data;
+        }
+        return $data;
+    }
+	
+    /*
+     * setter
+     */
+
+    public function set_akun($akun) {
+        $this->_akun = $akun;
+    }
+	
+    public function set_potongan_spm($potongan_spm) {
+        $this->_potongan_spm = $potongan_spm;
+    }
+	
+    public function set_setoran_mpn($setoran_mpn) {
+        $this->_setoran_mpn = $setoran_mpn;
+    }
+	public function set_total($total) {
+        $this->_total = $total;
+    }
+	
+	/*
+     * getter
+     */
+	
+	public function get_akun() {
+        return $this->_akun;
+    }
+		
+	public function get_potongan_spm() {
+        return $this->_potongan_spm;
+    }
+	
+	public function get_setoran_mpn() {
+        return $this->_setoran_mpn;
+    }
+	public function get_total() {
+        return $this->_total;
+    }
+    /*
+     * destruktor
+     */
+
+    public function __destruct() {
+        
+    }
+/*update*/
+}
---
+++ app/view/kppn/test.php
@@ -0,0 +1,162 @@
+<div id="top">
+	<div id="header">
+        <h2>Monitoring PFK Bulan <?php echo ($this->d_bulan); ?> <?php //echo $kode_satker; ?>
+			<?php //echo Session::get('user'); ?>
+		</h2>
+    
+<table><tr><td width="90%">
+</div>
+
+<form method="POST" action="GR_PFK" enctype="multipart/form-data">
+	<select type="text" name="bulan" id="bulan" style="margin-top: 10px; padding-top: 5px; float: right">
+			<option value='januari' <?php if ($this->d_bulan=='januari'){echo "selected";}?> >Januari</option>
+			<option value='februari' <?php if ($this->d_bulan=='februari'){echo "selected";}?> >Februari</option>
+			<option value='maret' <?php if ($this->d_bulan=='maret'){echo "selected";}?> >Maret</option>
+			<option value='april' <?php if ($this->d_bulan=='april'){echo "selected";}?> >April</option>
+			<option value='mei' <?php if ($this->d_bulan=='mei'){echo "selected";}?> >Mei</option>
+			<option value='juni' <?php if ($this->d_bulan=='juni'){echo "selected";}?> >Juni</option>
+			<option value='juli' <?php if ($this->d_bulan=='juli'){echo "selected";}?> >Juli</option>
+			<option value='agustus' <?php if ($this->d_bulan=='agustus'){echo "selected";}?> >Agustus</option>
+			<option value='september' <?php if ($this->d_bulan=='september'){echo "selected";}?> >September</option>
+			<option value='oktober' <?php if ($this->d_bulan=='oktober'){echo "selected";}?> >Oktober</option>
+			<option value='november' <?php if ($this->d_bulan=='november'){echo "selected";}?> >November</option>
+			<option value='desember' <?php if ($this->d_bulan=='desember'){echo "selected";}?> >Desember</option>
+			<!--option value='Validated' <?php //if ($this->status==Validated){echo "selected";}?>>Validated</option>
+			<option value='Error' <?php //if ($this->status==Error){echo "selected";}?>>Error</option-->
+			
+		</select></td>
+<td><input id="submit" class="sukses" type="submit" name="submit_file" value="CARI" onClick="return cek_upload();"></td>
+</tr>
+</form>
+</table>
+
+
+<!--table cellpadding="0" cellspacing="0" border="0" class="display KeyTable" id="example">
+	<thead>
+		<tr>
+			<th>Rendering engine</th>
+			<th>Browser</th>
+			<th>Platform(s)</th>
+			<th>Engine version</th>
+			<th>CSS grade</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="gradeX">
+			<td>Trident</td>
+			<td>Internet
+				 Explorer 4.0</td>
+			<td>Win 95+</td>
+			<td class="center">4</td>
+			<td class="center">X</td>
+		</tr-->
+
+
+
+<div id="fitur">
+		<table width="100%" class="table table-bordered zebra" id='fixheader'>
+            <!--baris pertama-->
+			<thead>
+					<th>No.</th>
+					<th>Akun</th>
+					<th>Potongan SPM</th>
+					<th>Setoran MPN</th>
+					<th>Total</th>
+			</thead>
+			<tbody class='ratatengah'>
+			<?php 
+			$no=1;
+			$tot_pot_spm=0;
+			$tot_set_mpn=0;
+			$tot_total=0;
+			//var_dump ($this->data);
+			if (isset($this->data)){
+				if (empty($this->data)){
+					echo "<div class='alert alert-danger'><strong>Info! </strong>Tidak ada data.</div>";
+				} else {
+			foreach ($this->data as $value){ 
+				echo "<tr>	";
+					echo "<td>" . $no++ . "</td>";
+					echo "<td>" . $value->get_akun() . "</td>";
+					echo "<td align='right'>" . number_format($value->get_potongan_spm()) . "</td>";
+					echo "<td align='right'>" . number_format($value->get_setoran_mpn()) . "</td>";
+					echo "<td align='right'>" . number_format($value->get_total()) . "</td>";
+				echo "</tr>	";
+				$tot_pot_spm = $tot_pot_spm  + $value->get_potongan_spm() ;
+				$tot_set_mpn = $tot_set_mpn  + $value->get_setoran_mpn() ;
+				$tot_total 	= $tot_total  + $value->get_total() ;
+			}
+				echo "<tr>	";
+					echo "<td></td>";
+					echo "<td><b>GRAND TOTAL</td>";
+					echo "<td align='right'><b>" . number_format($tot_pot_spm) . "</td>";
+					echo "<td align='right'><b>" . number_format($tot_set_mpn) . "</td>";
+					echo "<td align='right'><b>" . number_format($tot_total)  . "</td>";
+				echo "</tr>	";
+			}
+			}
+			else {
+				echo "<div class='alert alert-info'><strong>Info! </strong>Silakan masukan filter.</div>";
+			}
+			?>
+			</tbody>
+        </table>
+		</div>
+</div>
+</div>
+<script type="text/javascript" charset="utf-8" src="<?php echo URL; ?>public/js/jquery.js"></script>
+<script type="text/javascript" charset="utf-8" src="<?php echo URL; ?>public/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf-8">
+    $(function(){
+        hideErrorId();
+        hideWarning();
+        
+    });
+    
+    function hideErrorId(){
+        $('.error').fadeOut(0);
+    }
+
+    function hideWarning(){
+		$('#status').change(function(){
+            if(document.getElementById('status').value !=''){
+                $('#wstatus').fadeOut(200);
+            }
+        });
+
+    }
+    
+    function cek_upload(){
+		var v_status = document.getElementById('status').value;
+		
+        var jml = 0;
+		if(v_status==''){
+			$('#wstatus').html('Harap pilih');
+            $('#wstatus').fadeIn();
+            jml++;
+        }
+		if(jml>0){
+            return false;
+        } 
+    }
+	
+	$(document).ready( function () {
+		var oTable = $('#example').dataTable( {
+			"sScrollY": "300px",
+			"sScrollX": "100%",
+			"sScrollXInner": "110%",
+			"bSort": false,
+			"bPaginate": false,
+			"bInfo": null,
+			"bFilter": false,
+			"oLanguage": {
+			"sEmptyTable": "Tidak ada data di dalam tabel ini."
+			},
+		} );
+				
+		var keys = new KeyTable( {
+			"table": document.getElementById('example'),
+			"datatable": oTable
+		} );
+	} );
+</script>
