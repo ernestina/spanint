3f469340d894d23144d55af9e3a03ba78431efd7
hello.rchan <hello.rchan@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-06-24 02:53:49 +0000

    half-baked

--- app/models/DataDashboard.php
+++ app/models/DataDashboard.php
@@ -11,11 +11,11 @@ class DataDashboard {
     
     private $_gaji;
     private $_non_gaji;
-    private $_retur;
+    private $_lainnya;
     private $_void;
     private $_vol_gaji;
     private $_vol_non_gaji;
-    private $_vol_retur;
+    private $_vol_lainnya;
     private $_vol_void;
     
     private $_vol_completed;
@@ -31,6 +31,9 @@ class DataDashboard {
     private $_jenis_sp2d;
     private $_check_number;
     private $_nominal_sp2d;
+    
+    private $_retur_belum_proses;
+    private $_retur_sudah_proses;
     
     public $registry;
 
@@ -48,27 +51,48 @@ class DataDashboard {
         
         $data = array();
         for ($i=0; $i<$hari; $i++) {
-            $sql = "select jenis_sp2d, count(check_number) jumlah, sum(amount) nominal from (select distinct(check_number), jenis_sp2d, amount from AP_CHECKS_ALL_V where substr(check_number,3,3) = ".Session::get('id_user')." and check_date = to_date('".date("Ymd",time()-($i*24*60*60))."','yyyymmdd')) group by jenis_sp2d";
+            $sql = "select status_lookup_code, jenis_sp2d, count(check_number) jumlah, sum(amount) nominal from (select distinct(check_number), status_lookup_code, jenis_sp2d, amount from AP_CHECKS_ALL_V where substr(check_number,3,3) = '".Session::get('id_user')."' and check_date = to_date('".date("Ymd",time()-($i*24*60*60))."','yyyymmdd')) group by status_lookup_code, jenis_sp2d";
             $result =  $this->db->select($sql);
+            //var_dump($result);
             $d_data = new $this($this->registry);
             foreach ($result as $val) {
-                if ($val['JENIS_SP2D']=='GAJI') {
-                    $d_data->set_gaji($val['JUMLAH']);
-                    $d_data->set_vol_gaji($val['NOMINAL']);
-                } else if ($val['JENIS_SP2D']=='NON GAJI') {
-                    $d_data->set_non_gaji($val['JUMLAH']);
-                    $d_data->set_vol_non_gaji($val['NOMINAL']);
-                } else if ($val['JENIS_SP2D']=='RETUR') {
-                    $d_data->set_retur($val['JUMLAH']);
-                    $d_data->set_vol_retur($val['NOMINAL']);
-                } else if ($val['JENIS_SP2D']=='VOID') {
-                    $d_data->set_void($val['JUMLAH']);
-                    $d_data->set_vol_void($val['NOMINAL']);
+                if ($val['STATUS_LOOKUP_CODE']=='VOIDED') {
+                    $d_data->set_void($d_data->get_void() + $val['JUMLAH']);
+                    $d_data->set_vol_void($d_data->get_vol_void() + $val['NOMINAL']);
+                } else {
+                    if ($val['JENIS_SP2D']=='GAJI') {
+                        $d_data->set_gaji($d_data->get_gaji() + $val['JUMLAH']);
+                        $d_data->set_vol_gaji($d_data->get_vol_gaji() + $val['NOMINAL']);
+                    } else if ($val['JENIS_SP2D']=='NON GAJI') {
+                        $d_data->set_non_gaji($d_data->get_non_gaji() + $val['JUMLAH']);
+                        $d_data->set_vol_non_gaji($d_data->get_vol_non_gaji() + $val['NOMINAL']);
+                    } else if ($val['JENIS_SP2D']=='LAINNYA') {
+                        $d_data->set_lainnya($d_data->get_lainnya() + $val['JUMLAH']);
+                        $d_data->set_vol_lainnya($d_data->get_vol_lainnya() + $val['NOMINAL']);
+                    }
                 }
             }
             $data[$i] = $d_data;
         }
+        //var_dump($data);
         return $data;
+    }
+    
+    public function get_sp2d_retur($hari) {
+        $sql = "select STATUS_RETUR, count(STATUS_RETUR) JUMLAH from RETUR_SPAN_V where KDKPPN='".Session::get('id_user')."' and TGL_RETUR between to_date('".date("Ymd",time()-(($hari-1)*24*60*60))."','yyyymmdd') and to_date('".date("Ymd",time())."','yyyymmdd') group by STATUS_RETUR;";
+        var_dump($sql);
+        $result =  $this->db->select($sql);
+        var_dump($result);
+        $d_data = new $this($this->registry);
+        foreach ($result as $val) {
+            if ($val['STATUS_RETUR']=='SUDAH PROSES') {
+                $d_data->set_retur_sudah_proses($d_data->get_retur_sudah_proses() + $val['JUMLAH']);
+            } else {
+                $d_data->set_retur_belum_proses($d_data->get_retur_belum_proses() + $val['JUMLAH']);
+            }
+        }
+        var_dump($d_data);
+        return $d_data;
     }
     
     public function get_list_sp2d_selesai() {
@@ -158,8 +182,8 @@ class DataDashboard {
     public function set_void($void) {
         $this->_void = $void;
     }
-    public function set_retur($retur) {
-        $this->_retur = $retur;
+    public function set_lainnya($lainnya) {
+        $this->_lainnya = $lainnya;
     }
     public function set_vol_gaji($vol_gaji) {
         $this->_vol_gaji = $vol_gaji;
@@ -170,8 +194,8 @@ class DataDashboard {
     public function set_vol_void($vol_void) {
         $this->_vol_void = $vol_void;
     }
-    public function set_vol_retur($vol_retur) {
-        $this->_vol_retur = $vol_retur;
+    public function set_vol_lainnya($vol_lainnya) {
+        $this->_vol_lainnya = $vol_lainnya;
     }
     
     //SP2D Ongoing
@@ -226,6 +250,13 @@ class DataDashboard {
     public function set_nominal_sp2d($nominal_sp2d) {
         $this->_nominal_sp2d = $nominal_sp2d;
     }
+    
+    public function set_retur_sudah_proses($retur_sudah_proses) {
+        $this->_retur_sudah_proses = $retur_sudah_proses;
+    }
+    public function set_retur_belum_proses($retur_belum_proses) {
+        $this->_retur_belum_proses = $retur_belum_proses;
+    }
 		
 	/*
      * getter
@@ -241,8 +272,8 @@ class DataDashboard {
     public function get_void() {
         return $this->_void;
     }
-    public function get_retur() {
-        return $this->_retur;
+    public function get_lainnya() {
+        return $this->_lainnya;
     }
     public function get_vol_gaji() {
         return $this->_vol_gaji;
@@ -253,8 +284,8 @@ class DataDashboard {
     public function get_vol_void() {
         return $this->_vol_void;
     }
-    public function get_vol_retur() {
-        return $this->_vol_retur;
+    public function get_vol_lainnya() {
+        return $this->_vol_lainnya;
     }
     
     //SP2D Ongoing
@@ -308,6 +339,13 @@ class DataDashboard {
     }
     public function get_nominal_sp2d() {
         return $this->_nominal_sp2d;
+    }
+    
+    public function get_retur_sudah_proses() {
+        return $this->_retur_sudah_proses;
+    }
+    public function get_retur_belum_proses() {
+        return $this->_retur_belum_proses;
     }
     
     /*
