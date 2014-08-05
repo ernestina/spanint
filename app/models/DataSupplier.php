0e1575e0f50236a870d74e6911da4aa28281e19e
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-08-05 05:19:29 +0000

    menambahkan halaman data supplier

---
+++ app/models/DataSupplier.php
@@ -0,0 +1,336 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class DataSupplier {
+
+    private $db;
+	private $_nama_supplier;
+	private $_npwp_supplier;
+	private $_kdvalas;
+	private $_nm_bank;
+	private $_cabang;
+	private $_kd_bank;
+	private $_kd_swift;
+	private $_iban;
+	private $_asal_bank;
+	private $_norek_bank;
+	private $_norek_penerima;
+	private $_nm_pemilik_rek;
+	private $_npwp_penerima;
+	private $_nip_penerima;
+	private $_nm_penerima;
+	private $_tipe_supp;
+	private $_satker;
+	private $_v_supplier_number;
+	private $_kppn_code;
+	private $_email;
+	private $_alamat;
+	private $_city;
+	private $_provinsi;
+	private $_negawa;
+	private $_zip;
+	private $_phone;
+	private $_update_date;
+	private $_ids;
+    private $_error;
+    private $_valid = TRUE;
+    private $_table = 'SUPP';
+    public $registry;
+
+    /*
+     * konstruktor
+     */
+
+    public function __construct($registry = Registry) {
+        $this->db = $registry->db;
+        $this->registry = $registry;
+    }
+    
+    public function get_supp_filter($filter) {
+		$sql = "SELECT NAMA_SUPPLIER, NPWP_SUPPLIER, KDVALAS, NM_BANK, CABANG, KD_BANK
+				, KD_SWIFT, IBAN, ASAL_BANK, NOREK_BANK, NOREK_PENERIMA, NM_PEMILIK_REK
+				, NPWP_PENERIMA, NIP_PENERIMA, NM_PENERIMA, TIPE_SUPP
+				, SATKER, V_SUPPLIER_NUMBER, KPPN_CODE, EMAIL, ALAMAT
+				, CITY, PROVINSI, NEGARA, ZIP, PHONE, UPDATE_DATE, IDS
+				FROM " . $this->_table . "
+				WHERE 1 = 1";
+		$no=0;
+		//var_dump($filter);
+		foreach ($filter as $filter) {
+			$sql .= " AND ".$filter;
+		}
+		//var_dump ($sql);
+        $result = $this->db->select($sql);
+        $data = array();   
+        foreach ($result as $val) {
+            $d_data = new $this($this->registry);
+            $d_data->set_kppn_code($val['KPPN_CODE']);
+            $d_data->set_v_supplier_number($val['V_SUPPLIER_NUMBER']);
+            $d_data->set_tipe_supp($val['TIPE_SUPP']);
+            $d_data->set_nama_supplier($val['NAMA_SUPPLIER']);
+            $d_data->set_npwp_supplier($val['NPWP_SUPPLIER']);
+            $d_data->set_nm_bank($val['NM_BANK']);
+            $d_data->set_asal_bank($val['ASAL_BANK']);
+            $d_data->set_kdvalas($val['KDVALAS']);
+            $d_data->set_nm_penerima($val['NM_PENERIMA']);
+            $d_data->set_nm_pemilik_rek($val['NM_PEMILIK_REK']);
+            $d_data->set_norek_penerima($val['NOREK_PENERIMA']);
+            $d_data->set_kd_swift($val['KD_SWIFT']);
+            $d_data->set_iban($val['IBAN']);
+            $d_data->set_npwp_penerima($val['NPWP_PENERIMA']);
+            $d_data->set_nip_penerima($val['NIP_PENERIMA']);
+			$data[] = $d_data;
+        }
+        return $data;
+    }
+	
+    /*
+     * setter
+     */
+
+    public function set_nama_supplier($nama_supplier) {
+        $this->_nama_supplier = $nama_supplier;
+    }
+
+    public function set_npwp_supplier($npwp_supplier) {
+        $this->_npwp_supplier = $npwp_supplier;
+    }
+
+    public function set_kdvalas($kdvalas) {
+        $this->_kdvalas = $kdvalas;
+    }
+
+    public function set_nm_bank($nm_bank) {
+        $this->_nm_bank = $nm_bank;
+    }
+
+    public function set_cabang($cabang) {
+        $this->_cabang = $cabang;
+    }
+
+    public function set_kd_bank($kd_bank) {
+        $this->_kd_bank = $kd_bank;
+    }
+
+    public function set_kd_swift($kd_swift) {
+        $this->_kd_swift = $kd_swift;
+    }
+
+    public function set_iban($iban) {
+        $this->_iban = $iban;
+    }
+
+    public function set_asal_bank($asal_bank) {
+        $this->_asal_bank = $asal_bank;
+    }
+
+    public function set_norek_bank($norek_bank) {
+        $this->_norek_bank = $norek_bank;
+    }
+
+    public function set_norek_penerima($norek_penerima) {
+        $this->_norek_penerima = $norek_penerima;
+    }
+
+    public function set_nm_pemilik_rek($nm_pemilik_rek) {
+        $this->_nm_pemilik_rek = $nm_pemilik_rek;
+    }
+
+    public function set_npwp_penerima($npwp_penerima) {
+        $this->_npwp_penerima = $npwp_penerima;
+    }
+
+    public function set_nip_penerima($nip_penerima) {
+        $this->_nip_penerima = $nip_penerima;
+    }
+
+    public function set_nm_penerima($nm_penerima) {
+        $this->_nm_penerima = $nm_penerima;
+    }
+
+    public function set_tipe_supp($tipe_supp) {
+        $this->_tipe_supp = $tipe_supp;
+    }
+
+    public function set_satker($satker) {
+        $this->_satker = $satker;
+    }
+
+    public function set_v_supplier_number($v_supplier_number) {
+        $this->_v_supplier_number = $v_supplier_number;
+    }
+
+    public function set_kppn_code($kppn_code) {
+        $this->_kppn_code = $kppn_code;
+    }
+
+    public function set_email($email) {
+        $this->_email = $email;
+    }
+
+    public function set_alamat($alamat) {
+        $this->_alamat = $alamat;
+    }
+
+    public function set_city($city) {
+        $this->_city = $city;
+    }
+
+    public function set_provinsi($provinsi) {
+        $this->_provinsi = $provinsi;
+    }
+
+    public function set_negara($negara) {
+        $this->_negara = $negara;
+    }
+
+    public function set_zip($zip) {
+        $this->_zip = $zip;
+    }
+
+    public function set_phone($phone) {
+        $this->_phone = $phone;
+    }
+
+    public function set_update_date($update_date) {
+        $this->_update_date = $update_date;
+    }
+
+    public function set_ids($ids) {
+        $this->_ids = $ids;
+    }
+	
+		
+	/*
+     * getter
+     */
+	
+	public function get_nama_supplier() {
+        return $this->_nama_supplier;
+    }
+	
+	public function get_npwp_supplier() {
+        return $this->_npwp_supplier;
+    }
+	
+	public function get_kdvalas() {
+        return $this->_kdvalas;
+    }
+	
+	public function get_nm_bank() {
+        return $this->_nm_bank;
+    }
+	
+	public function get_cabang() {
+        return $this->_cabang;
+    }
+	
+	public function get_kd_bank() {
+        return $this->_kd_bank;
+    }
+	
+	public function get_kd_swift() {
+        return $this->_kd_swift;
+    }
+	
+	public function get_iban() {
+        return $this->_iban;
+    }
+	
+	public function get_asal_bank() {
+        return $this->_asal_bank;
+    }
+	
+	public function get_norek_bank() {
+        return $this->_norek_bank;
+    }
+	
+	public function get_norek_penerima() {
+        return $this->_norek_penerima;
+    }
+	
+	public function get_nm_pemilik_rek() {
+        return $this->_nm_pemilik_rek;
+    }
+	
+	public function get_npwp_penerima() {
+        return $this->_npwp_penerima;
+    }
+	
+	public function get_nip_penerima() {
+        return $this->_nip_penerima;
+    }
+	
+	public function get_nm_penerima() {
+        return $this->_nm_penerima;
+    }
+	
+	public function get_tipe_supp() {
+        return $this->_tipe_supp;
+    }
+	
+	public function get_satker() {
+        return $this->_satker;
+    }
+	
+	public function get_v_supplier_number() {
+        return $this->_v_supplier_number;
+    }
+	
+	public function get_kppn_code() {
+        return $this->_kppn_code;
+    }
+	
+	public function get_email() {
+        return $this->_email;
+    }
+	
+	public function get_alamat() {
+        return $this->_alamat;
+    }
+	
+	public function get_city() {
+        return $this->_city;
+    }
+	
+	public function get_provinsi() {
+        return $this->_provinsi;
+    }
+	
+	public function get_negara() {
+        return $this->_negara;
+    }
+	
+	public function get_zip() {
+        return $this->_zip;
+    }
+	
+	public function get_phone() {
+        return $this->_phone;
+    }
+	
+	public function get_update_date() {
+        return $this->_update_date;
+    }
+	
+	public function get_ids() {
+        return $this->_ids;
+    }
+	
+	public function get_table() {
+        return $this->_table;
+    }
+
+    /*
+     * destruktor
+     */
+
+    public function __destruct() {
+        
+    }
+
+}
---
+++ app/view/satker/isianSupplier.php
@@ -0,0 +1,263 @@
+<div id="top">
+	<div id="header">
+        <h2>CEK SUPPLIER <br>
+			
+		</h2>
+    </div>
+
+<a href="#oModal" class="modal">FILTER DATA</a>
+        <div id="oModal" class="modalDialog" >
+            <div>
+                <h2 style="border-bottom: 1px solid #eee; padding-bottom: 10px">FILTER</h2>
+				<a href="<?php
+                    $_SERVER['PHP_SELF'];
+                ?>" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 5px; margin-top: 2px"></i>
+</a>
+
+	<div id="top">	
+		<form method="POST" action="cekSupplier" enctype="multipart/form-data">
+		
+		<div id='wtipesup' class='error'></div>
+		<label class='isian'>Tipe Supplier: </label>
+		<select type='text' name='tipesup' id='tipesup'>
+			<option value='1' <?php if ($this->d_tipesup==1){echo "selected";}?>>1. Satker</option>
+			<option value='2' <?php if ($this->d_tipesup==2){echo "selected";}?>>2. Penyedia BJ</option>
+			<option value='3' <?php if ($this->d_tipesup==3){echo "selected";}?>>3. Pegawai</option>
+			<option value='4' <?php if ($this->d_tipesup==4){echo "selected";}?>>4. BA.999</option>
+			<option value='5' <?php if ($this->d_tipesup==5){echo "selected";}?>>5. Transfer Daerah</option>
+			<option value='6' <?php if ($this->d_tipesup==6){echo "selected";}?>>6. PP/Banyak Penerima</option>
+			<option value='7' <?php if ($this->d_tipesup==7){echo "selected";}?>>7. Lain-lain</option>
+		</select>
+		
+		<div id="wnrs" class="error"></div>
+		<label class="isian">NRS: </label>
+		<input type="number" name="nrs" id="nrs" size="15" value="<?php if (isset($this->d_nrs)){echo $this->d_nrs;}?>">
+		
+		<div id="wnamasupplier" class="error"></div>
+		<label class="isian">Nama Supplier: </label>
+		<input type="text" name="namasupplier" id="namasupplier" value="<?php if (isset($this->d_namasupplier)){echo $this->d_namasupplier;}?>">
+		
+		<div id="wnpwpsupplier" class="error"></div>
+		<label class="isian">NPWP Supplier: </label>
+		<input type="text" name="npwpsupplier" id="npwpsupplier" value="<?php if (isset($this->d_npwpsupplier)){echo $this->d_npwpsupplier;}?>">
+		
+		<div id="wnip" class="error"></div>
+		<label class="isian">NIP Penerima: </label>
+		<input type="number" name="nip" id="nip" size="18" value="<?php if (isset($this->d_nip)){echo $this->d_nip;}?>">
+		
+		<div id="wnamapenerima" class="error"></div>
+		<label class="isian">Nama Penerima: </label>
+		<input type="text" name="namapenerima" id="namapenerima" value="<?php if (isset($this->d_namapenerima)){echo $this->d_namapenerima;}?>">
+		
+		<div id="wnorek" class="error"></div>
+		<label class="isian">Nomor Rek.: </label>
+		<input type="text" name="norek" id="norek" value="<?php if (isset($this->d_norek)){echo $this->d_norek;}?>">
+		
+		<div id="wnamarek" class="error"></div>
+		<label class="isian">Nama Rek.: </label>
+		<input type="text" name="namarek" id="namarek" value="<?php if (isset($this->d_namarek)){echo $this->d_namarek;}?>">
+		
+		<div id="wnpwppenerima" class="error"></div>
+		<label class="isian">NPWP Penerima: </label>
+		<input type="text" name="npwppenerima" id="npwppenerima" value="<?php if (isset($this->d_npwppenerima)){echo $this->d_npwppenerima;}?>">
+
+		<ul class="inline" style="margin-left: 150px">
+		<li><input id="reset" class="normal" type="reset" name="reset_file" value="RESET" onClick=""></li>
+		<li><input id="submit" class="sukses" type="submit" name="submit_file" value="SUBMIT" onClick="return cek_upload();"></li>
+		</ul>
+	</form>
+</div>
+</div>
+</div>
+
+<div id="fitur">
+
+		<table width="100%" class="table table-bordered zebra" id='fixheader' style="font-size: 80%">
+            <!--baris pertama-->
+			<thead>
+					<th>No.</th>
+					<th>Kode KPPN</th>
+					<th>NRS / Tipe Kode Pos</th>
+					<th>Nama Supplier</th>
+					<th>NPWP Supplier</th>
+					<th>Nama Bank</th>
+					<th>KD NAB - Kode Valas</th>
+					<th>Nama Penerima</th>
+					<th>Nama pemilik Rekening</th>
+					<th>Nomor Rekening</th>
+					<th>SWIFT / IBAN</th>
+					<th>NPWP Penerima</th>
+					<th>NIP Penerima</th>
+					<th>pilih</th>
+					
+			</thead>
+			<tbody class='ratatengah'>
+			<?php 
+			$no=1;
+			if (isset($this->data)){
+				if (empty($this->data)){
+					echo "<div class='alert alert-danger'><strong>Info! </strong>Tidak ada data.</div>";
+				} else {
+					foreach ($this->data as $value){ 
+						echo "<tr>	";
+							echo "<td>" . $no++ . "</td>";
+							echo "<td>" . $value->get_kppn_code() . "</td>";
+							echo "<td>" . $value->get_v_supplier_number()." / ". $value->get_tipe_supp() . "</td>";
+							echo "<td>" . $value->get_nama_supplier() . "</td>";
+							echo "<td>" . $value->get_npwp_supplier() . "</td>";
+							echo "<td>" . $value->get_nm_bank() . "</td>";
+							echo "<td>" . $value->get_asal_bank()." - ".$value->get_kdvalas(). "</td>";
+							echo "<td>" . $value->get_nm_penerima() . "</td>";
+							echo "<td>" . $value->get_nm_pemilik_rek() . "</td>";
+							echo "<td>" . $value->get_norek_penerima() . "</td>";
+							echo "<td>" . $value->get_kd_swift()." / ".$value->get_iban() . "</td>";
+							echo "<td>" . $value->get_npwp_penerima() . "</td>";
+							echo "<td>" . $value->get_nip_penerima() . "</td>";
+							echo "<td><input type='checkbox' name='>".$value->get_v_supplier_number()."'> </td>";
+							
+						echo "</tr>	";
+					}
+				} 
+			} else {
+				echo "<div class='alert alert-info'><strong>Info! </strong>Silakan masukan filter.</div>";
+			}
+			?>
+			</tbody>
+        </table>
+		<b>Keterangan : </b></br>
+		Sukses Overbooking = Dana sudah masuk ke Rekening Penerima </br>
+		Sukses RTGS / SKN / Swift = Dana sudah ditransfer dari Bank Pembayar ke Bank Penerima, mekanisme transfer dana dari Bank Penerima ke Rekening Penerima tergantung pada Bank Penerima</br>
+		Nomor Ref/SOR/BOR = Nomor bukti transaksi pada perbankan yang dapat digunakan untuk konfirmasi ke bank penerima
+		</div>
+</div>
+
+<script type="text/javascript" charset="utf-8" src="<?php echo URL; ?>public/js/jquery.js"></script>
+<script type="text/javascript" charset="utf-8" src="<?php echo URL; ?>public/js/jquery.dataTables.js"></script>
+<script src="<?php echo URL; ?>public/js/jquery-ui.js"></script>
+<script type="text/javascript" charset="utf-8">
+    $(function(){
+        hideErrorId();
+        hideWarning();
+		
+	});
+	
+    function hideErrorId(){
+        $('.error').fadeOut(0);
+    }
+
+    function hideWarning(){
+		
+        $('#tipesup').keyup(function() {
+            if (document.getElementById('tipesup').value != '') {
+                $('#wtipesup').fadeOut(200);
+            }
+        })
+		
+		$('#nrs').keyup(function(){
+            if(document.getElementById('nrs').value !=''){
+                $('#wnrs').fadeOut(200);
+            }
+        });
+		
+		$('#namasupplier').keyup(function(){
+            if(document.getElementById('namasupplier').value !=''){
+                $('#wnamasupplier').fadeOut(200);
+            }
+        });
+		
+		$('#npwpsupplier').keyup(function(){
+            if(document.getElementById('npwpsupplier').value !=''){
+                $('#wnpwpsupplier').fadeOut(200);
+            }
+        });
+		
+		$('#nip').keyup(function(){
+            if(document.getElementById('nip').value !=''){
+                $('#wnip').fadeOut(200);
+            }
+        });
+		
+		$('#namapenerima').keyup(function(){
+            if(document.getElementById('namapenerima').value !=''){
+                $('#wnamapenerima').fadeOut(200);
+            }
+        });
+		
+		$('#norek').keyup(function(){
+            if(document.getElementById('norek').value !=''){
+                $('#wnorek').fadeOut(200);
+            }
+        });
+		
+		$('#namarek').keyup(function(){
+            if(document.getElementById('namarek').value !=''){
+                $('#wnamarek').fadeOut(200);
+            }
+        });
+		
+		$('#npwppenerima').keyup(function(){
+            if(document.getElementById('npwppenerima').value !=''){
+                $('#wnpwppenerima').fadeOut(200);
+            }
+        });
+
+    }
+	
+    function cek_upload(){
+		var pattern = '^[0-9]+$';
+		var v_tipesup = document.getElementById('tipesup').value;
+		var v_nrs = document.getElementById('nrs').value;
+		var v_namasupplier = document.getElementById('namasupplier').value;
+		var v_npwpsupplier = document.getElementById('npwpsupplier').value;
+		var v_nip = document.getElementById('nip').value;
+		var v_namapenerima = document.getElementById('namapenerima').value;
+		var v_norek = document.getElementById('norek').value;
+		var v_namarek = document.getElementById('namarek').value;
+		var v_npwppenerima = document.getElementById('npwppenerima').value;
+		
+        var jml = 0;
+		
+		if(v_tipesup =='2' && v_norek==''){
+            var wnorek = 'Untuk tipe supplier 2, nomor rekening harus diisi';
+            $('#wnorek').html(wnorek);
+            $('#wnorek').fadeIn(200);
+            jml++;
+        }
+		
+		if(v_tipesup =='1' || v_tipesup =='3'){
+            document.getElementById("namasupplier").value='<?php echo Session::get('kd_satker');?>';
+        }
+		
+		if((v_tipesup =='4' || v_tipesup =='5' || v_tipesup =='6' || v_tipesup =='7')&& v_npwpsupplier==''){
+            var wnpwpsupplier = 'Untuk tipe supplier selain 1,2 atau 3, npwp harus diisi';
+            $('#wnpwpsupplier').html(wnpwpsupplier);
+            $('#wnpwpsupplier').fadeIn(200);
+            jml++;
+        }
+		
+        if(jml>0){
+            return false;
+        } 
+    }
+	
+	$(document).ready( function () {
+		var oTable = $('#fixheader').dataTable( {
+			"sScrollY": 400,
+			"sScrollX": "100%",
+			"sScrollXInner": "100%",
+			"bSort": false,
+			"bPaginate": false,
+			"bInfo": null,
+			"bFilter": false,
+			"oLanguage": {
+			"sEmptyTable": "Tidak ada data di dalam tabel ini."
+			
+			},
+		} );
+				
+		var keys = new KeyTable( {
+			"table": document.getElementById('fixheader'),
+			"datatable": oTable
+		} );
+	} );
+</script>
