bfeb526ad599887a98d893f0389b169931d8cb0b
creativehardbeat1 <creativehardbeat1@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-08-19 13:52:10 +0000

    Update Model DataRealisasi.php

--- trunk/app/models/DataRealisasi.php
+++ trunk/app/models/DataRealisasi.php
@@ -352,7 +352,7 @@ class DataRealisasi{
 		//$sql .= " group by substr(a.program,1,3), b.nmba " ;
 		$sql .= " ORDER by b.kdsatker " ;
 		
-		var_dump ($sql);
+		//var_dump ($sql);
         $result = $this->db->select($sql);
         $data = array();   
         foreach ($result as $val) {
