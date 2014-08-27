cd55449df28ea309d91810705e10cfe4f9c17ff7
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-08-27 05:17:29 +0000

    tambah lg sedikt

--- trunk/app/models/DataLog.php
+++ trunk/app/models/DataLog.php
@@ -63,19 +63,19 @@ class DataLog {
 		if (!empty($_SERVER['HTTP_CLIENT_IP'])){
 			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
 		}
-		else if((!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+		else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		}
-		else if((!empty($_SERVER['HTTP_X_FORWARDED'])){
+		else if(!empty($_SERVER['HTTP_X_FORWARDED'])){
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
 		}
-		else if((!empty($_SERVER['HTTP_FORWARDED_FOR'])){
+		else if(!empty($_SERVER['HTTP_FORWARDED_FOR'])){
 			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
 		}
-		else if((!empty($_SERVER['HTTP_FORWARDED'])){
+		else if(!empty($_SERVER['HTTP_FORWARDED'])){
 			$ipaddress = $_SERVER['HTTP_FORWARDED'];
 		}
-		else if((!empty($_SERVER['REMOTE_ADDR'])){
+		else if(!empty($_SERVER['REMOTE_ADDR'])){
 			$ipaddress = $_SERVER['REMOTE_ADDR'];
 		}
 		else {
