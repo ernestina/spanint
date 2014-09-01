30386154a6f62330671d01ec3ac6dbabaeb78844
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-09-01 03:02:16 +0000

    memperbaiki download supplier untuk kppn besar

--- trunk/app/view/satker/downloadSupplierxls.php
+++ trunk/app/view/satker/downloadSupplierxls.php
@@ -1,30 +1,37 @@
 <?php
-$filename = "datasupplier_".$this->kppn_code.$this->ekstensi;
 
-header("Content-Type: application/octet-stream");
-header("Content-Disposition: ,Filename=".$filename);
-
-
-if (isset($this->data)){
-		foreach ($this->data as $value){
-			echo trim($value->get_nama_supplier())."|";
-			echo trim($value->get_npwp_supplier())."|";
-			echo trim($value->get_kdvalas())."|";
-			echo trim($value->get_nm_bank())."|";
-			echo trim($value->get_kd_bank())."|";
-			echo trim($value->get_kd_swift())."|";
-			echo trim($value->get_iban())."|";
-			echo trim($value->get_asal_bank())."|";
-			echo trim($value->get_norek_bank())."|";
-			echo trim($value->get_norek_penerima())."|";
-			echo trim($value->get_nm_pemilik_rek())."|";
-			echo trim($value->get_npwp_penerima())."|";
-			echo trim($value->get_nip_penerima())."|";
-			echo trim($value->get_nm_penerima())."|";
-			echo trim($value->get_tipe_supp())."|";
-			echo trim($value->get_satker())."|";
-			echo trim($value->get_v_supplier_number())."|";
-			echo trim($value->get_kppn_code())." \r\n";
-	}
+if (!ini_get('safe_mode')) {
+    set_time_limit(0);
+    ini_set('max_input_time', -1);
+    ini_set('memory_limit', -1);
+    ini_set('max_execution_time', -1);
 }
+
+	$server_ftp ='10.100.93.134';
+	$user_ftp ='spanuat:spanuat';
+	$direktori='APLIKASI/CEKSUPPLIER/data_supplier_kppn';
+	$filename = "datasupplier_" . $this->kppn_code . $this->ekstensi;
+	header("Content-Type: application/octet-stream");
+	header("Content-Disposition: ,Filename=" . $filename);
+	header('Content-Transfer-Encoding: binary');
+	ob_end_clean();
+	
+	//setting alamat download dari ftp, contoh : $ftp_address = "ftp://spanuat:spanuat@10.100.93.134/APLIKASI/CEKSUPPLIER/data_supplier_kppn/datasupplier_088.xlsx";
+	//ftp://10.100.93.134/APLIKASI/CEKSUPPLIER/data_supplier_kppn/datasupplier_088.txt
+	
+	$ftp_address = "ftp://".$user_ftp."@".$server_ftp."/".$direktori."/".$filename;
+	
+	//$download_addres="ftp://".$user_ftp."@".$server_ftp."/".$direktori."/".$filename;
+	
+	
+	$file = $ftp_address;
+	$fd=fopen($file,"r");
+	
+	while (!feof($fd)){
+		$buffer=fread($fd,1028);
+		echo $buffer;
+	}
+	fclose($fd);
+	exit;
+	
 ?>
