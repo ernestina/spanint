81c3247cf8de764c3fba6ca8cb6b0397b61cfa53
hello.rchan@gmail.com <hello.rchan@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-09-01 22:30:08 +0000

    BETTER MOBILE SUPPORT BRAH

--- app/view/Header.php
+++ app/view/Header.php
@@ -5,7 +5,7 @@
         
         <meta charset="utf-8">
         
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=yes">
         
         <title>Online Monitoring SPAN</title>
         
