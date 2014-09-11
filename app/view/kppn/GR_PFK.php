934372d76592c7362c2e8b40c0b6b11000266df9
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-09-11 04:02:27 +0000

    memperbaiki sedikit kesalahan di cetak pdf penerimaan setoran PFK

--- app/view/kppn/GR_PFK.php
+++ app/view/kppn/GR_PFK.php
@@ -2,74 +2,68 @@
 <div class="main-window-segment" style="padding-top: none; padding-bottom: 20px;">
     <div class="container-fluid">
         <div class="row">
-            
+
             <div class="col-lg-10 col-md-6 col-sm-12">
                 <h2>Monitoring PFK Akun</h2>
             </div>
-            
+
             <div class="col-lg-1 col-md-3 col-sm-12" style="padding-top: 20px;">
-                         <?php
+                <?php
 //----------------------------------------------------
 //Development History.Revisi : 0 Kegiatan :1.mencetak hasil filter ke dalam pdf Dibuat oleh : Rifan Abdul Rachman Tanggal dibuat : 18-07-2014  File yang diubah : fund_fail.php  
-		
-			if (isset($this->data)) {
-				foreach ($this->data as $value) {
-					$kdbulan=substr($value->get_tanggal_bayar(),3,2);
-				  $kdakun = $value->get_akun();
-				   $kdkppn= SUBSTR($value->get_ntpn(),2,3);
-				}
 
-			}
-			?>
-			<a href="<?php echo URL; ?>PDF/GR_PFK_DETAIL1_PDF/<?php echo $kdakun . "/" . $kdbulan . "/" . $kdkppn; ?>" style="width: 100%" class="btn btn-default"><span class="glyphicon glyphicon-print"></span> PDF</a>
-			<?php
-						
-        
-
-	
-			//----------------------------------------------------		
-		        
-        ?>
+                if (isset($this->data)) {
+                    foreach ($this->data as $value) {
+                        $kdbulan = substr($value->get_tanggal_bayar(), 3, 2);
+                        $kdakun = $value->get_akun();
+                        $kdkppn = $value->get_kppn();
+                    }
+                }
+                ?>
+                <a href="<?php echo URL; ?>PDF/GR_PFK_DETAIL1_PDF/<?php echo $kdakun . "/" . $kdbulan . "/" . $kdkppn; ?>" style="width: 100%" class="btn btn-default"><span class="glyphicon glyphicon-print"></span> PDF</a>
+                <?php
+                //----------------------------------------------------		
+                ?>
 
             </div>
             <div class="col-lg-1 col-md-3 col-sm-12" style="padding-top: 20px;">
-            
-                <!-- button type="button" style="width: 100%" class="btn btn-default" data-toggle="modal" data-target="#modal-app-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button -->
-                
+
+    <!-- button type="button" style="width: 100%" class="btn btn-default" data-toggle="modal" data-target="#modal-app-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button -->
+
             </div>
         </div>
-        
-        <div class="row" style="padding-top: 10px">
-            
-            <div class="col-md-6 col-sm-12">
-                <?php
-                    $akun = '';
 
-                    foreach ($this->data as $value) {
-                        $akun = $value->get_akun();
-                    }
-                    echo $akun;
-                    if (isset($this->d_nama_kppn)) {
-                        foreach ($this->d_nama_kppn as $kppn) {
-                            echo $kppn->get_nama_user() . " (" . $kppn->get_kd_satker() . ")";
-                        }
-                    }
-                    ?>
+        <div class="row" style="padding-top: 10px">
+
+            <div class="col-md-6 col-sm-12">
+<?php
+$akun = '';
+
+foreach ($this->data as $value) {
+    $akun = $value->get_akun();
+}
+echo $akun;
+if (isset($this->d_nama_kppn)) {
+    foreach ($this->d_nama_kppn as $kppn) {
+        echo $kppn->get_nama_user() . " (" . $kppn->get_kd_satker() . ")";
+    }
+}
+?>
             </div>
-            
+
             <div class="col-md-6 col-sm-12" style="text-align: right;">
                 <?php
-                    // untuk menampilkan last_update
-                    if (isset($this->last_update)) {
-                        foreach ($this->last_update as $last_update) {
-                            echo "Update Data Terakhir (Waktu Server)<br/>" . $last_update->get_last_update() . " WIB";
-                        }
+                // untuk menampilkan last_update
+                if (isset($this->last_update)) {
+                    foreach ($this->last_update as $last_update) {
+                        echo "Update Data Terakhir (Waktu Server)<br/>" . $last_update->get_last_update() . " WIB";
                     }
+                }
                 ?>
             </div>
-            
+
         </div>
-        
+
     </div>
 </div>
 
@@ -77,58 +71,58 @@
 <div id="table-container" class="wrapper">
     <table class="footable">
         <!--baris pertama-->
-                <thead>
-                    <tr>
-                    <th>No.</th>
+        <thead>
+            <tr>
+                <th>No.</th>
                 <th>Sumber Transaksi</th>
                 <th>Nama wajib Bayar setor</th>
                 <th>Tanggal Buku</th>
                 <th>Tanggal Bayar</th>
                 <th>NTPN/SP2D</th>
                 <th>Rupiah</th>
-                    </tr>
-                </thead>
-                <tbody class='ratatengah'>
-                    <?php
-                    $no = 1;
-                    $tot_pot = 0;
+            </tr>
+        </thead>
+        <tbody class='ratatengah'>
+<?php
+$no = 1;
+$tot_pot = 0;
 
-                    //var_dump ($this->data);
-                    if (isset($this->data)) {
-                        if (empty($this->data)) {
-                            echo '<td colspan=12 align="center">Tidak ada data.</td>';
-                        } else {
-                            foreach ($this->data as $value) {
-                                echo "<tr>	";
-                                echo "<td>" . $no++ . "</td>";
-                                echo "<td>" . $value->get_keterangan() . "</td>";
-                                echo "<td>" . $value->get_nama_wajib_bayar_setor() . "</td>";
-                                echo "<td>" . $value->get_tanggal_buku() . "</td>";
-                                echo "<td>" . $value->get_tanggal_bayar() . "</td>";
-                                echo "<td>" . $value->get_ntpn() . "</td>";
-                                echo "<td align='right'>" . number_format($value->get_rupiah()) . "</td>";
-                                echo "</tr>	";
-                                $tot_pot = $tot_pot + $value->get_rupiah();
-                            }
-                        }
-                    }
-                    ?>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan='5'></td>
-                        <td class='ratatengah'><b>GRAND TOTAL</td>
-                        <td align='right'><b><?php echo number_format($tot_pot); ?>
-                        </td>
+//var_dump ($this->data);
+if (isset($this->data)) {
+    if (empty($this->data)) {
+        echo '<td colspan=12 align="center">Tidak ada data.</td>';
+    } else {
+        foreach ($this->data as $value) {
+            echo "<tr>	";
+            echo "<td>" . $no++ . "</td>";
+            echo "<td>" . $value->get_keterangan() . "</td>";
+            echo "<td>" . $value->get_nama_wajib_bayar_setor() . "</td>";
+            echo "<td>" . $value->get_tanggal_buku() . "</td>";
+            echo "<td>" . $value->get_tanggal_bayar() . "</td>";
+            echo "<td>" . $value->get_ntpn() . "</td>";
+            echo "<td align='right'>" . number_format($value->get_rupiah()) . "</td>";
+            echo "</tr>	";
+            $tot_pot = $tot_pot + $value->get_rupiah();
+        }
+    }
+}
+?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan='5'></td>
+                <td class='ratatengah'><b>GRAND TOTAL</td>
+                <td align='right'><b><?php echo number_format($tot_pot); ?>
+                </td>
 
-                    </tr>
-                </tfoot>
+            </tr>
+        </tfoot>
     </table>
 </div>
 
 <!-- Filter -->
 <div class="modal fade" id="modal-app-filter" tabindex="-1" role="dialog" aria-labelledby="app-filter-label" aria-hidden="true">
-        
+
     <div class="modal-dialog">
 
         <div class="modal-content">
@@ -139,36 +133,44 @@
                 <h4 class="modal-title" id="app-filter-label"><span class="glyphicon glyphicon-filter"></span> Filter Data</h4>
 
             </div>
-            
+
             <form id="filter-form" method="POST" action="sp2dRekap" enctype="multipart/form-data">
 
                 <div class="modal-body">
-                    
-                    <!-- Paste Isi Fom mulai nangkene -->
-                    <?php if (isset($this->kppn_list)) { ?>
-                    <div id="wkdkppn" class="alert alert-danger" style="display:none;"></div>
-                    <label class="isian">Kode KPPN: </label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-home"></span></span>
-                        <select class="form-control" type="text" name="kdkppn" id="kdkppn">
-                        <?php foreach ($this->kppn_list as $value1){ 
-                        if ($kode_kppn==$value1->get_kd_d_kppn()){echo "<option value='".$value1->get_kd_d_kppn()."' selected>".$value1->get_kd_d_kppn()." | ".$value1->get_nama_user()."</option>";} 
-                        else {echo "<option value='".$value1->get_kd_d_kppn()."'>".$value1->get_kd_d_kppn()." | ".$value1->get_nama_user()."</option>";}
 
-                        } ?>
-                        </select>
-                    </div>
-                    <?php } ?>
+                    <!-- Paste Isi Fom mulai nangkene -->
+<?php if (isset($this->kppn_list)) { ?>
+                        <div id="wkdkppn" class="alert alert-danger" style="display:none;"></div>
+                        <label class="isian">Kode KPPN: </label>
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-home"></span></span>
+                            <select class="form-control" type="text" name="kdkppn" id="kdkppn">
+                        <?php
+                        foreach ($this->kppn_list as $value1) {
+                            if ($kode_kppn == $value1->get_kd_d_kppn()) {
+                                echo "<option value='" . $value1->get_kd_d_kppn() . "' selected>" . $value1->get_kd_d_kppn() . " | " . $value1->get_nama_user() . "</option>";
+                            } else {
+                                echo "<option value='" . $value1->get_kd_d_kppn() . "'>" . $value1->get_kd_d_kppn() . " | " . $value1->get_nama_user() . "</option>";
+                            }
+                        }
+                        ?>
+                            </select>
+                        </div>
+<?php } ?>
                     <br/>
                     <div id="wtgl" class="alert alert-danger" style="display:none;"></div>
                     <label class="isian">Tanggal: </label>
-                    
+
                     <div class="input-daterange input-group" id="datepicker" style="width: 100%">
-                        <input class="form-control" type="text" class="tanggal" name="tgl_awal" id="tgl_awal" value="<?php if (isset($this->d_tgl_awal)){echo $this->d_tgl_awal;}?>">
+                        <input class="form-control" type="text" class="tanggal" name="tgl_awal" id="tgl_awal" value="<?php if (isset($this->d_tgl_awal)) {
+    echo $this->d_tgl_awal;
+} ?>">
                         <span class="input-group-addon">s.d.</span>
-                        <input class="form-control" type="text" class="tanggal" name="tgl_akhir" id="tgl_akhir" value="<?php if (isset($this->d_tgl_akhir)){echo $this->d_tgl_akhir;}?>">
+                        <input class="form-control" type="text" class="tanggal" name="tgl_akhir" id="tgl_akhir" value="<?php if (isset($this->d_tgl_akhir)) {
+    echo $this->d_tgl_akhir;
+} ?>">
                     </div>
-                        
+
 
                 </div>
 
