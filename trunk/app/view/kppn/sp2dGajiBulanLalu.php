cb02ea97d807794ccfe25cc6afbceedd15d53690
creativehardbeat1 <creativehardbeat1@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-12-19 06:55:32 +0000

    Update report

--- trunk/app/view/kppn/sp2dGajiBulanLalu.php
+++ trunk/app/view/kppn/sp2dGajiBulanLalu.php
@@ -13,33 +13,33 @@
 			//----------------------------------------------------
 			//Development History.Revisi : 0 Kegiatan :1.mencetak hasil filter ke dalam pdf Dibuat oleh : Rifan Abdul Rachman Tanggal dibuat : 18-07-2014  File yang diubah : posisiSPM.php  
 				if (Session::get('role') == ADMIN || Session::get('role') == KANWIL) {
-				    if( isset($this->d_nama_kppn) ){
-						if (isset($this->d_nama_kppn)) {
-							foreach ($this->d_nama_kppn as $kppn) {
-								$kdkppn = $kppn->get_kd_satker();
-							  }
-						} else {
-							$kdkppn = Session::get('id_user');
-						}
-                ?>
-                <a href="<?php echo URL; ?>PDF/sp2dCompareGaji_PDF/<?php echo $kdkppn; ?>" style="width: 100%" class="btn btn-default"><span class="glyphicon glyphicon-print"></span> PDF</a>
-                <?php
-                //----------------------------------------------------		
+					if (isset($this->d_nama_kppn)) {
+						foreach ($this->d_nama_kppn as $kppn) {
+							$kdkppn = $kppn->get_kd_satker();
+						  }
+					} else {
+						$kdkppn = 'null';
+					}
+					if (isset($this->d_tahun)) {
+						$kdtahun=$this->d_tahun;
+					} else {
+						$kdtahun= 'null';
 					}
 				}
 				
 				if (Session::get('role') == KPPN) {
-							$kdkppn = Session::get('id_user');
+						$kdkppn = Session::get('id_user');
+						if (isset($this->d_tahun)) {
+							$kdtahun=$this->d_tahun;
+						} else {
+							$kdtahun= 'null';
+						}
 
-                ?>
-                <a href="<?php echo URL; ?>PDF/sp2dCompareGaji_PDF/<?php echo $kdkppn; ?>" style="width: 100%" class="btn btn-default"><span class="glyphicon glyphicon-print"></span> PDF</a>
-
-                <?php
-
-				
 				}
-
+				
                 ?>
+				<a href="<?php echo URL; ?>PDF/sp2dCompareGaji_PDF/<?php echo $kdkppn . "/" . $kdtahun;; ?>" style="width: 100%" class="btn btn-default"><span class="glyphicon glyphicon-print"></span> PDF</a>
+				
                 
                 
             </div>
