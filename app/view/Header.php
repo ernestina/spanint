8b2850e091912c03eaa8359d28bd1d3420aa1c94
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@085aa4da-177b-b8d5-9481-d58475f4679e>
2014-12-22 04:48:16 +0000

    dikit

--- app/view/Header.php
+++ app/view/Header.php
@@ -736,7 +736,14 @@
                     <ul>
 
                         <li class="nav"><h4><a href="<?php echo URL; ?>home"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Beranda</a></h4></li>
-                        <li class="nav"><h4><a href="<?php echo URL; ?>dataDroping/monitoringDroping"><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Penyaluran dan Droping Dana SP2D</a></h4></li>
+
+                        <li class="subnav"><h4><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Modul Kas</h4>
+                            <div class="collapse">
+                                <ul>
+                                    <li><a href="<?php echo URL; ?>dataDroping/monitoringDroping">Penyaluran dan Droping Dana SP2D</a></li>
+                                </ul>
+                            </div>
+                        </li>
 
                     </ul>
 
